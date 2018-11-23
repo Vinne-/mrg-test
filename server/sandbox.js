7f9cdafa72d521631c0ddeac84638cd4950504ac
Vincent <vincent.cottin@gmail.com>
2018-11-23 14:54:55 +0100

    endpoints done

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,9 +1,17 @@
 const R = require('ramda');
 
-const allGames = require('./all-games.json');
+const allGames = require('./data/all-games.json');
+const gameCollectionIds = require('./data/gameCollectionIds.json');
 
 const uniqueGamesCollectionIds = [];
 
-allGames.forEach((game) => {
-	console.log(game.gameCollectionIds);
+
+// allGames.forEach((game) => {
+// 	console.log(game.gameCollectionIds);
+// });
+
+gameCollectionIds.forEach(id => {
+	let newName = id.replace(/-/g, '_');
+	newName = newName.toUpperCase();
+	console.log(newName);
 });
